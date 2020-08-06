93f6d46ccbbe203a52f3f42bd24c9be19b0b4256
heenkoko <loveeeeunju@gmail.com>
2020-08-06 12:14:35 +0900

    mybatis 연동

--- jex00/src/main/java/org/zerock/config/RootConfig.java
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -4,6 +4,9 @@ package org.zerock.config;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,19 +16,33 @@ import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"org.zerock.sample"})
+@MapperScan(basePackages = {"org.zerock.mapper"})
 public class RootConfig
 {
     @Bean
     public DataSource dataSource()
     {
         HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-        hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+        // log4jdbc 적용 전
+//        hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+//        hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+        
+        // logo4jdbc 적용 후
+        hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+        hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:XE");
         hikariConfig.setUsername("book_ex");
         hikariConfig.setPassword("book_ex");
         
         HikariDataSource dataSource = new HikariDataSource(hikariConfig);
         
         return dataSource;
+    }
+    
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception
+    {
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(dataSource());
+        return (SqlSessionFactory) sqlSessionFactory.getObject();
     }
 }
