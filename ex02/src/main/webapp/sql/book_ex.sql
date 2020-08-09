2febb7f8359ef0f1d54697e6b9bedb9110d2c580
heenkoko <loveeeeunju@gmail.com>
2020-08-09 16:09:41 +0900

    MVC Controller

--- ex02/src/main/webapp/sql/book_ex.sql
+++ ex02/src/main/webapp/sql/book_ex.sql
@@ -15,3 +15,4 @@ alter table tbl_board add constraint pk_board primary key (bno);
 insert into tbl_board (bno, title, content, writer)
 values (seq_board.nextval, '테스트 제목', '테스트 내용', 'user00');
 
+select * from tbl_board;
