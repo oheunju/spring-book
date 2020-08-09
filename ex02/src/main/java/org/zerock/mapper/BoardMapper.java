43d3bee1e79e30340e9036aa0d19c0e93a9c785a
heenkoko <loveeeeunju@gmail.com>
2020-08-09 12:50:38 +0900

    MVC dao

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -2,7 +2,6 @@ package org.zerock.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Select;
 import org.zerock.domain.BoardVO;
 
 public interface BoardMapper
@@ -10,5 +9,14 @@ public interface BoardMapper
 //    @Select("select * from tbl_Board where bno > 0")
     public List<BoardVO> getList();
     
+    public void insert(BoardVO board);
+    
+    public void insertSelectKey(BoardVO board);
+    
+    public BoardVO read(Long bno);
+    
+    public int delete(Long bno);
+    
+    public int update(BoardVO board);
     
 }
