cc4abc5ba5dfb220e720c129db1d582c052adf45
heenkoko <loveeeeunju@gmail.com>
2020-08-12 08:52:44 +0900

    paging

--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -1,5 +1,7 @@
 package org.zerock.mapper;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import org.junit.runner.RunWith;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -18,6 +21,17 @@ public class BoardMapperTests
 {
     @Setter(onMethod_ = @Autowired)
     private BoardMapper mapper;
+    
+    @Test
+    public void testPaging()
+    {
+        Criteria cri = new Criteria();
+        cri.setPageNum(2);
+        cri.setAmount(10);
+        
+        List<BoardVO> list = mapper.getListWithPaging(cri);
+        list.forEach(board -> log.info(board));
+    }
     
     @Test
     public void testGetList()
--- jex02/src/test/java/com/zerock/controller/BoardControllerTests.java
+++ jex02/src/test/java/com/zerock/controller/BoardControllerTests.java
@@ -36,6 +36,15 @@ public class BoardControllerTests
     }
     
     @Test
+    public void testListPaging() throws Exception
+    {
+        log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/list")
+                 .param("pageNum", "2")
+                 .param("amount", "10"))
+                 .andReturn().getModelAndView().getModelMap());
+    }
+    
+    @Test
     public void testList() throws Exception
     {
         log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/list"))
