cc7d91ed9f3ac7ef49ed067009209ca3c508f039
heenkoko <loveeeeunju@gmail.com>
2020-08-17 11:02:10 +0900

    AOP

---
+++ ex04/src/main/java/org/zerock/config/RootConfig.java
@@ -0,0 +1,19 @@
+package org.zerock.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/*
+ * Java 설정 이용하는 경우
+ */
+//@Configuration
+//@ComponentScan(basePackages = {"org.zerock.service"})
+//@ComponentScan(basePackages = "org.zerock.aop")
+//@EnableAspectJAutoProxy
+//
+//@MapperScan(basePackages = {"org.zerock.mapper"})
+public class RootConfig
+{
+    
+}
